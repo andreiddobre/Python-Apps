963630b0de3ef9c10bab55d5375b0438c7262f38
Andrei Dobre <75906525+andreiddobre@users.noreply.github.com>
2023-04-02 11:58:39 +0300

    Add files via upload

---
+++ Image color extraction.py
@@ -0,0 +1,18 @@
+from PIL import Image
+Image.open('leaves.jpg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+image = mpimg.imread('leaves.jpg')
+w, h, d = tuple(image.shape)
+pixels = np.reshape(image, (w * h, d))
+from sklearn.cluster import KMeans
+n_colors = 3 #nr of colors
+model = KMeans(n_clusters = n_colors, random_state = 42).fit(pixels)
+palette = np.uint8(model.cluster_centers_)
+plt.imshow([palette])
+plt.show()
+
+#code inspired by clcoding.com
