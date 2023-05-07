5357a5c8cb21c752518d1bd7991d72eccf046c6b
Andrei Dobre <75906525+andreiddobre@users.noreply.github.com>
2023-05-07 22:26:43 +0300

    Add files via upload

---
+++ Decimal to binary, octal, hexa.py
@@ -0,0 +1,10 @@
+dec = int(input("Enter your number to be converted: "))
+
+#Decimal to Binary
+print(f'{dec} to binary is: {bin(dec)}.')
+
+#Decimal to Octal
+print(f'{dec} to octal is: {oct(dec)}.')
+
+#Decimal to Hexadecimal
+print(f'{dec} to hexadecimal is: {hex(dec)}.')
