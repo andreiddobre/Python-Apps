109584548eb32e7eac980bfb87fd13ae15a4a880
Andrei Dobre <75906525+andreiddobre@users.noreply.github.com>
2023-05-21 22:46:44 +0300

    Update Random password.py

--- Random password.py
+++ Random password.py
@@ -1,6 +1,7 @@
+#pip install random
 import random
 
-#User input for password length 
+#user input for password length 
 pass_length = int(input("Enter the length of password: "))
 
 #storing letters, numbers and special characters
@@ -8,4 +9,6 @@ all_char="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&
 
 #random sampling by joining the length of the password and the variable all_char
 generated_pass = "".join(random.sample(all_char, pass_length))
+
+#print generated_pass
 print(generated_pass)
