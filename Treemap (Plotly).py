6d2ada3669199479d34345e59afa0bdaac1121cf
Andrei Dobre <75906525+andreiddobre@users.noreply.github.com>
2023-05-25 23:35:39 +0300

    Update Treemap (Plotly).py

--- Treemap (Plotly).py
+++ Treemap (Plotly).py
@@ -3,9 +3,7 @@ import plotly.graph_objects as treePlot
 
 fig = treePlot.Figure(treePlot.Treemap(
     labels = ["A","B", "C", "D", "E", "F", "G", "H", "I"],
-    parents = ["", "A", "A", "C", "C", "A", "A", "G", "A"]
-    ))
-
+    parents = ["", "A", "A", "C", "C", "A", "A", "G", "A"]))
 fig.show()
 
 #original code inspired by clcoding.com
